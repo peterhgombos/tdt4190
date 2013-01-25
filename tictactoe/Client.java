7e9f6810f70b96a38be4fc1a3979cdb745fa18a2
lycantrophe <wolfhood@gmail.com>
2013-01-25 13:15:41 +0100

    Implements constructors and basic connection setup.

--- tictactoe/Client.java
+++ tictactoe/Client.java
@@ -1,20 +1,33 @@
-import Connection;
-import TicTacToeGui;
-
-package tictactoe;
+import java.rmi.*;
+import java.rmi.server.*;
+import java.util.List;
+import java.io.*;
 
 public class Client extends UnicastRemoteObject implements Connection {
 
     private Server server;
+    private TicTacToeGui gui;
 
-    public void mark( final List state ) throws RemoteException {
-        while( this.server.receive_state( state ) != SUCCESS );
+    public Client( final String address, TicTacToeGui gui )
+        throws ConnectException, NotBoundException, java.net.MalformedURLException, RemoteException {
+
+        this.gui = gui;
+        System.setSecurityManager( new LiberalSecurityManager() );
+        this.server = (Server)Naming.lookup( "rmi://" + address + "/ttt" );
+        System.out.println( "Connection set up with " + address );
     }
 
-    private boolean receive_state( final List state ) throws RemoteException {
+    public void mark( final List state ) throws RemoteException, IntegrityException {
+        while( this.server.receive_state( state ) != true );
+    }
+
+    public boolean receive_state( final List state ) throws RemoteException {
         /* Sets unconditionally as this is the slave */
-        for( final coord : state )
-            this.gui.setMark( coord.x, coord.y );
+        for( final Pair< Integer, Integer > coord : (List<Pair>)state ) {
+            this.gui.forceMark( coord.x, coord.y );
+        }
+
+        this.gui.repaint();
 
         return true;
     }
--- tictactoe/Server.java
+++ tictactoe/Server.java
@@ -1,24 +1,35 @@
-import Connection;
-import TicTacToeGui;
-import java.rmi.server;
-import Tuple;
-
-package tictactoe;
+import java.rmi.*;
+import java.rmi.server.*;
+import java.util.List;
+import java.io.*;
 
 public class Server extends UnicastRemoteObject implements Connection {
 
     private Client client;
+    private TicTacToeGui gui;
 
-    public void mark( final List state ) {
+    public Server( final String address, TicTacToeGui gui )
+        throws ConnectException, NotBoundException, java.net.MalformedURLException, RemoteException {
+        this.gui = gui;
+        /* sanity check address? */
+        System.setSecurityManager( new LiberalSecurityManager() );
+        Naming.rebind( "rmi://" + address + "/ttt", this );
+        System.out.println( "Registry set up at " + address );
+    }
+
+    public void mark( final List state ) throws RemoteException, IntegrityException {
 
         /* draw please wait */
 
-        while( this.client.receive_state( state ) != SUCCESS );
+        while( !this.client.receive_state( state ) );
     }
 
-    private boolean receive_state( final List state ) throws RemoteException {
+    public boolean receive_state( final List state ) throws RemoteException, IntegrityException {
         /* integrity check */
-    }
+        if( state.size() != 1 ) throw new IntegrityException();
+        Pair< Integer, Integer > coord = (Pair)state.get( 0 );
+        this.gui.setMark( coord.x, coord.y );
 
-    public void setup();
+        return true;
+    }
 }
