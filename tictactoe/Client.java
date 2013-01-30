b151dd6b9bba40d5323694f8b83b8935268cdbcc
lycantrophe <lycantrophe@lavabit.com>
2013-01-30 16:02:52 +0100

    Better implementation of peer-to-peer model.
    Introduces a better interface with needed operations both locally and remotely.
    
    Solves plenty of problems with the old model by making both peers needing both
    a local server and a local client session. Both instances uses the same
    interface, and should be fairly invisible from a caller standpoint.

--- tictactoe/Client.java
+++ tictactoe/Client.java
@@ -5,17 +5,25 @@ import java.io.*;
 
 public class Client extends UnicastRemoteObject implements Connection {
 
-    private Server server;
+    private Connection server;
 
     public Client( final String address )
         throws ConnectException, NotBoundException, java.net.MalformedURLException, RemoteException {
 
         System.setSecurityManager( new LiberalSecurityManager() );
-        this.server = (Server)Naming.lookup( "rmi://" + address + "/ttt" );
+        this.server = (Connection)Naming.lookup( "rmi://" + address + "/ttt" );
         System.out.println( "Connection set up with " + address );
     }
 
-    public void mark( final Pair coord ) throws RemoteException, IntegrityException {
+    public void mark( final Pair coord ) throws RemoteException {
         this.server.mark( coord );
+    }
+
+    public boolean register( final String address ) throws RemoteException {
+        return this.server.register( address );
+    }
+
+    public boolean new_game() throws RemoteException {
+        return this.server.new_game();
     }
 }
--- tictactoe/Server.java
+++ tictactoe/Server.java
@@ -7,17 +7,29 @@ public class Server extends UnicastRemoteObject implements Connection {
 
     private TicTacToeGui gui;
 
-    public Server( final String address, TicTacToeGui gui )
+    public Server( TicTacToeGui gui )
         throws ConnectException, NotBoundException, java.net.MalformedURLException, RemoteException {
         this.gui = gui;
-        /* sanity check address? */
+
+        final String address = "localhost";
         System.setSecurityManager( new LiberalSecurityManager() );
         Naming.rebind( "rmi://" + address + "/ttt", this );
         System.out.println( "Registry set up at " + address );
     }
 
-    public void mark( final Pair coord ) throws RemoteException, IntegrityException {
-        Pair< Integer, Integer > coord = (Pair)state.get( 0 );
-        this.gui.setMark( coord.x, coord.y );
+    public void mark( final Pair coord ) throws RemoteException {
+        this.gui.setMark( (Integer)coord.x, (Integer)coord.y );
+    }
+
+    public boolean register( final String address ) throws RemoteException {
+        return this.gui.prompt_connection( address );
+    }
+
+    public boolean new_game() throws RemoteException {
+        if( !this.gui.prompt_newgame() )
+            return false;
+
+        this.gui.clearBoard();
+        return true;
     }
 }
