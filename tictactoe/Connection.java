2752340b6c3d454eef740be678edd977c8faf978
lycantrophe <lycantrophe@lavabit.com>
2013-01-30 16:02:52 +0100

    Better implementation of peer-to-peer model.
    Introduces a better interface with needed operations both locally and remotely.
    
    Solves plenty of problems with the old model by making both peers needing both
    a local server and a local client session. Both instances uses the same
    interface, and should be fairly invisible from a caller standpoint.

--- tictactoe/Connection.java
+++ tictactoe/Connection.java
@@ -4,4 +4,6 @@ import java.util.List;
 public interface Connection extends Remote {
     /* Sends mark to this or client depending on role */
     public void mark( final Pair coord ) throws RemoteException;
+    public boolean register( final String address ) throws RemoteException;
+    public boolean new_game() throws RemoteException;
 }
